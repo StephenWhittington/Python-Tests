aba79a9bd4fcbcd08fcaeaf2008f8868be69ba8b
Ubuntu <ubuntu@ip-172-31-88-17.ec2.internal>
2019-09-23 21:33:15 +0000

    Second python test passed

--- python.py
+++ python.py
@@ -15,7 +15,7 @@ def even_number_of_evens(numbers):
         return evens % 2 == 0
 
 assert even_number_of_evens([]) == False, "No numbers"   
-# assert even_number_of_evens([2]) == False, "One even number"
+assert even_number_of_evens([2]) == False, "One even number"
 # assert even_number_of_evens([2, 4]) == True, "Two even numbers"
 # assert even_number_of_evens([2, 3]) == False, "Two numbers, only one even"
 # assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8]) == False, "Multiple numbers, three even"
